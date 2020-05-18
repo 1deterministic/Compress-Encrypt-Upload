b40fd6510a645cb0d555672a48e04f4738261f7f
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-05-18 17:07:18 -0300

    Added a combined compress-and-encrypt script that prevents having an unencrypted .tar.gz file

--- install.sh
+++ install.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-if [[ $(whoami) == "root" ]]; then echo "Please run this script as a regular user"; echo "Closing"; exit 1; fi
+# config file
+mkdir -p "$HOME/.config/ceu"
+cp "./config" "$HOME/.config/ceu"
 
-echo "CEU requires tar, gzip, gpg and rclone with a gpg key and a rclone remote both available, you can pause here and check that before running. If everything is ok, type OK (all caps)"
-read answer
-if [[ ! "$answer" == "OK" ]]; then echo "Closing"; exit 1; fi
+# executables
+sudo cp "./compress" "/usr/bin"
+sudo cp "./encrypt" "/usr/bin"
+sudo cp "./compress-and-encrypt" "/usr/bin"
+sudo cp "./upload" "/usr/bin"
 
-mkdir "$HOME/.config"
-mkdir "$HOME/.config/systemd"
-mkdir "$HOME/.config/systemd/user"
-mkdir "$HOME/.local"
-mkdir "$HOME/.local/bin"
-
-# changes ./config to $HOME/.config/ceu and sends the main files to $HOME/.local/bin
-sed "s|./config|$HOME/.config/ceu|g" "compress" > "$HOME/.local/bin/compress"; chmod +x "$HOME/.local/bin/compress"
-sed "s|./config|$HOME/.config/ceu|g" "encrypt" > "$HOME/.local/bin/encrypt"; chmod +x "$HOME/.local/bin/encrypt"
-sed "s|./config|$HOME/.config/ceu|g" "upload" > "$HOME/.local/bin/upload"; chmod +x "$HOME/.local/bin/upload"
-
-# installs the config file
-cp "config" "$HOME/.config/ceu"; chmod +x "$HOME/.config/ceu"
-
-# installs the systemd user service
-sed "s|/path/to/compress|$HOME/.local/bin/compress|g;s|/path/to/encrypt|$HOME/.local/bin/encrypt|g;s|/path/to/upload|$HOME/.local/bin/upload|g" "ceu.service" > "$HOME/.config/systemd/user/ceu.service"
-cp "ceu.timer" "$HOME/.config/systemd/user/ceu.timer"
+# systemd service and timer
+mkdir -p "$HOME/.config/systemd/user"
+cp "./ceu.service" "$HOME/.config/systemd/user"
+cp "./ceu.timer" "$HOME/.config/systemd/user"
 systemctl --user daemon-reload
 systemctl --user enable ceu.timer
 
-echo "Now you can edit $HOME/.config/ceu with the values you want"
-echo "You can also run loginctl enable-linger $USER as root if you want to allow the timer to run even if $USER is not logged in"
+# allow user services to run without login
+sudo loginctl enable-linger "$USER"
+
+# start the timer right away
+systemctl --user start ceu.timer
--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if [ $(whoami) == "root" ]; then echo "Please run this script as a regular user"; exit 1; fi
+# stop the timer
+systemctl --user stop ceu.timer
 
+# disallow user services to run without login 
+sudo loginctl disable-linger "$USER"
+
+# systemd service and timer
 systemctl --user disable ceu.timer
-rm "$HOME/.config/systemd/user/ceu.service"
-rm "$HOME/.config/systemd/user/ceu.timer"
+rm "./ceu.service" "$HOME/.config/systemd/user"
+rm "./ceu.timer" "$HOME/.config/systemd/user"
 systemctl --user daemon-reload
 
-rm "$HOME/.local/bin/compress"
-rm "$HOME/.local/bin/encrypt"
-rm "$HOME/.local/bin/upload"
-rm "$HOME/.config/ceu"
+# config file (uncomment to remove the config file)
+# rm -r "$HOME/.config/ceu"
 
-echo "Removed all CEU files but the compressed and encrypted files were not touched"
-echo "If you enabled the user the run services without login with loginctl enable-linger $USER you may want to disable that with loginctl disable-linger $USER as root"
+# executables
+sudo rm "/usr/bin/compress"
+sudo rm "/usr/bin/encrypt"
+sudo rm "/usr/bin/compress-and-encrypt"
+sudo rm "/usr/bin/upload"
